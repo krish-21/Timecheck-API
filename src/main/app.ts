4fbdfef6676d879c68bdb73ed255c6c254c2d2a9
Krishna Nanduri <krishna.21nv@gmail.com>
2023-05-10 21:47:16 +0800

    TA-28: Integrate Helmet & Express Rate Limit

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,8 +1,11 @@
 import cors from "cors";
 import morgan from "morgan";
+import helmet from "helmet";
 import express from "express";
+import { rateLimit } from "express-rate-limit";
 
 import { appConfig } from "main/utils/environment/AppConfig";
+import { MINUTES, SECONDS, MILLISECONDS } from "main/utils/time";
 
 import { handleError } from "main/middleware/handleError";
 import { isAuthenticated } from "main/middleware/isAuthenticated";
@@ -19,6 +22,18 @@ export const app = express();
 //
 //
 
+app.disable("x-powered-by");
+
+const limiter = rateLimit({
+  windowMs: MINUTES.FIFTEEN * SECONDS.MINUTE * MILLISECONDS.SECOND,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(limiter);
+
+app.use(helmet());
 app.use(cors());
 
 //
---
+++ src/main/utils/time.ts
@@ -0,0 +1,23 @@
+export const MILLISECONDS = {
+  SECOND: 1000,
+};
+
+export const SECONDS = {
+  ZERO: 0,
+  ONE: 1,
+  TEN: 10,
+  THIRTY: 30,
+  MINUTE: 60,
+  HOUR: 3600,
+  DAY: 86400,
+};
+
+export const MINUTES = {
+  FIFTEEN: 15,
+};
+
+export const DAYS = {
+  WEEK: 7,
+  YEAR: 365,
+  LEAP_YEAR: 366,
+};
